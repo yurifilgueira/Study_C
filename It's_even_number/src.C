536fa1190b16780079b7800a5c0414c37ef1738b
Yuri Filgueira <yurimedeiros141@gmail.com>
2022-09-15 21:05:13 -0300

    It's_even_number

---
+++ It's_even_number/src.C
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int ehPar(int numb)
+{
+    if (numb % 2 == 0)
+    {
+        bool aws = true;
+        return (aws);
+    }
+    else
+    {
+        bool aws = false;
+        return (aws);
+    }
+}
+
+int main()
+{
+    int numb;
+
+    scanf("%d", &numb);
+
+    bool aws = ehPar(numb);
+
+    if (aws == true)
+    {
+        printf("true");
+    }
+    else
+    {
+        printf("false");
+    }
+
+    return (0);
+}
